50788e678a01cf2cdd359c72896acaee37a523cb
OmarNajjar-Dev <najjar_omar@outlook.com>
2025-04-29 18:32:51 +0300

    Rename file name

--- src/pages/Checkout/index.jsx
+++ src/pages/Checkout/index.jsx
@@ -3,7 +3,7 @@ import ShippingForm from "./ShippingForm";
 import PaymentForm from "./PaymentForm";
 import OrderSummary from "./OrderSummary";
 import { Elements } from "@stripe/react-stripe-js";
-import stripePromise from "../../srtipe"; 
+import stripePromise from "../../stripe"; 
 
 export default function Checkout() {
   const [formData, setFormData] = useState({
