072db31ff0f7b6957839d133ecdaecec0fab20db
OmarNajjar-Dev <najjar_omar@outlook.com>
2025-04-29 20:29:08 +0300

    Edit the category from the home page

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import "./index.css";
 import App from "./App.jsx";
-import { AuthProvider } from "./contexts/AuthContext.js";
-import { CartProvider } from "./contexts/CartContext.js";
-import { CategoryProvider } from "./contexts/CategoryContext.jsx";
+import { AuthProvider } from "./contexts/AuthContext";
+import { CartProvider } from "./contexts/CartContext";
+import { CategoryProvider } from "./contexts/CategoryContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- src/pages/Checkout/index.jsx
+++ src/pages/Checkout/index.jsx
@@ -3,7 +3,7 @@ import ShippingForm from "./ShippingForm";
 import PaymentForm from "./PaymentForm";
 import OrderSummary from "./OrderSummary";
 import { Elements } from "@stripe/react-stripe-js";
-import stripePromise from "../../stripe"; 
+import stripePromise from "../../str    ipe"; 
 
 export default function Checkout() {
   const [formData, setFormData] = useState({
